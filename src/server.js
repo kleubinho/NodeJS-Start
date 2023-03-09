1496475102d1d566ee67a6087d7c6e76054edeec
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-03-09 17:33:20 -0300

    entendendo rotas

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((request, response) => {
-    return response.end('Hello Recena')
-})
+  const { method, url } = request;
 
-server.listen(3333)
+  if(method === 'GET' && url === '/users'){
+    //Early return => se o código bater no return abaixo, na que vem depois será executado
+    return response.end('Listagem de usuários') //end para retornar um texto
+  }
+
+  if(method === 'POST' && url === '/users'){
+    return response.end('Criação de usuários')
+  }
+  
+  return response.end("Hello World");
+});
+
+server.listen(3333);
