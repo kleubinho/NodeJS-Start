6fd1a0438b8ccf5966d3cab84c5e332a13bf837b
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-03-10 14:03:36 -0300

    update

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,7 +1,7 @@
 //process.stdin // Recebe de entrada no terminal
 //.pipe(process.stdout); //aqui encaminho(PIPE) para uma saida (stdout é uma stream de saida)
 
-import { Readable } from "node:stream";
+import { Readable, Transform, Writable } from "node:stream";
 
 class OneToHundredStream extends Readable {
   index = 1;
@@ -22,4 +22,21 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout);
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * -1
+
+        callback(null, Buffer.from(transformed.toString()))
+    }
+}
+
+class MultipleByTenStream extends Writable {
+    _write(chunk, encoding, callback) {
+        console.log(Number(chunk.toString()) * 10)
+        callback() //Encerrar tudo após ser executado
+    }
+}
+
+new OneToHundredStream() // Stream de leitura conseguimos apenas ler dados
+.pipe(new InverseNumberStream()) // Stream de transformação obrigatoriamente precisa ler dados de algum lugar e escrever dados para outro lugar
+.pipe(new MultipleByTenStream()); // Stream de escrita só conseguimos escrever dados para ela
