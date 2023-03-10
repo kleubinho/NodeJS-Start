8ec6c6fd8625cfadf2cfabf4c48d4ffab2164c86
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-03-10 00:59:40 -0300

    add headers

--- src/server.js
+++ src/server.js
@@ -1,17 +1,27 @@
 const http = require("http");
 
+const users = [];
+
 const server = http.createServer((request, response) => {
   const { method, url } = request;
 
-  if(method === 'GET' && url === '/users'){
+  if (method === "GET" && url === "/users") {
     //Early return => se o código bater no return abaixo, na que vem depois será executado
-    return response.end('Listagem de usuários') //end para retornar um texto
+    return response
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users)); //end para retornar um texto
   }
 
-  if(method === 'POST' && url === '/users'){
-    return response.end('Criação de usuários')
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "KLeber",
+      email: "kleber@gmail.com",
+    });
+
+    return response.end("Criação de usuários");
   }
-  
+
   return response.end("Hello World");
 });
 
